022db716daa0bb57e169fc6b0c03d04b691027c2
primaryhealthcareATM <primaryhealthcareatm@gmail.com>
2025-03-20 15:36:17 +0530

    Update atm_server.js

--- atm_server.js
+++ atm_server.js
@@ -88,7 +88,7 @@ app.post('/tablets', async (req, res) => {
     
     if (!name) return res.status(400).json({ error: "Tablet name is required" });
     
-    let tabList = name.split(' ');
+    let tabList = name.split(/[\n\s]+/);
     console.log('tabList: ', tabList);
 
     try {
